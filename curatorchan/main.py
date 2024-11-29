1bb9afb09ad21bf08a608e93afa988f6e4bbc3be
Oliver Harcourt <96662442+oliverharcourt@users.noreply.github.com>
2024-11-29 15:01:35 +0100

    Formatting changes

--- curatorchan/main.py
+++ curatorchan/main.py
@@ -3,10 +3,9 @@ from typing import Literal
 
 import discord
 import pandas as pd
+from anime_recommender.main import AnimeRecommender
 from discord.ext import commands
 from dotenv import load_dotenv
-
-from anime_recommender.main import AnimeRecommender
 
 DESCRIPTION = """
 Curator-chan is a Discord bot that recommends anime to users.
@@ -15,11 +14,7 @@ Curator-chan is a Discord bot that recommends anime to users.
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(
-    command_prefix="!",
-    description=DESCRIPTION,
-    intents=intents
-)
+bot = commands.Bot(command_prefix="!", description=DESCRIPTION, intents=intents)
 
 
 @bot.event
@@ -34,42 +29,39 @@ async def on_ready():
 
 
 def generate_recommendations(mode: str, search_string: str) -> pd.DataFrame:
-
     result = None
 
-    if mode == 'user':
-        result = AnimeRecommender(config_path="config.json",
-                                  search_str=search_string,
-                                  anime_mode=False,
-                                  limit=10).run()
+    if mode == "user":
+        result = AnimeRecommender(
+            config_path="config.json",
+            search_str=search_string,
+            anime_mode=False,
+            limit=10,
+        ).run()
 
-    elif mode == 'anime':
-        result = AnimeRecommender(config_path="config.json",
-                                  search_str=search_string,
-                                  anime_mode=True,
-                                  limit=10).run(autoselect=True)
+    elif mode == "anime":
+        result = AnimeRecommender(
+            config_path="config.json",
+            search_str=search_string,
+            anime_mode=True,
+            limit=10,
+        ).run(autoselect=True)
 
     else:
         raise ValueError("Invalid mode. Choose 'user' or 'anime'.")
 
-    dataset = pd.read_json("data/raw/mal_anime_data.json", orient='records')
+    dataset = pd.read_json("data/raw/mal_anime_data.json", orient="records")
 
-    merged_result = result.merge(dataset, on='id')
+    merged_result = result.merge(dataset, on="id")
 
-    cols = [
-        "title",
-        "distance",
-        "link",
-        "mean",
-        "nsfw"
-    ]
+    cols = ["title", "distance", "link", "mean", "nsfw"]
 
     merged_result = merged_result[cols]
 
-    merged_result['distance'] = merged_result["distance"].apply(
+    merged_result["distance"] = merged_result["distance"].apply(
         lambda x: int(round(x, 2) * 10)
     )
-    return merged_result.to_dict(orient='records')
+    return merged_result.to_dict(orient="records")
 
 
 def make_embed(recommendation: dict) -> discord.Embed:
@@ -79,34 +71,26 @@ def make_embed(recommendation: dict) -> discord.Embed:
     color = discord.Color.from_rgb(255, 215, 0)
     nsfw = "🔞 YES" if recommendation["nsfw"] == "black" else "✅ NO"
     embed = discord.Embed(
-        title=recommendation['title'],
-        url=recommendation['link'],
+        title=recommendation["title"],
+        url=recommendation["link"],
         description="",
-        color=color
+        color=color,
     )
-    embed.add_field(
-        name="Match", value=f"{recommendation['distance']}%", inline=False
-    )
+    embed.add_field(name="Match", value=f"{recommendation['distance']}%", inline=False)
     embed.add_field(
         name="Mean Score", value=f"{round(recommendation['mean'], 2)}", inline=True
     )
-    embed.add_field(
-        name="NSFW", value=f"{nsfw}", inline=True
-    )
+    embed.add_field(name="NSFW", value=f"{nsfw}", inline=True)
     return embed
 
 
-@ bot.tree.command(name='recommend', description='Get personalized recommendations.')
-@ discord.app_commands.describe(
-    mode='Select recommendation mode (user or anime).',
-    search_string='Enter the username or anime name.'
-
-
+@bot.tree.command(name="recommend", description="Get personalized recommendations.")
+@discord.app_commands.describe(
+    mode="Select recommendation mode (user or anime).",
+    search_string="Enter the username or anime name.",
 )
 async def recommend(
-    interaction: discord.Interaction,
-    mode: Literal['user', 'anime'],
-    search_string: str
+    interaction: discord.Interaction, mode: Literal["user", "anime"], search_string: str
 ):
     """Get recommendations based on mode and search string."""
     await interaction.response.defer(ephemeral=True)
