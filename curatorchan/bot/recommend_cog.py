d1fd44c89f878066bb2ea47812ace5c880f701cd
Oliver Harcourt <96662442+oliverharcourt@users.noreply.github.com>
2024-12-24 00:47:38 +0100

    Add logging setup to RecommendationCog for improved debugging

--- curatorchan/bot/recommend_cog.py
+++ curatorchan/bot/recommend_cog.py
@@ -152,4 +152,20 @@ class RecommendationCog(commands.Cog):
 
 
 async def setup(bot):
-    await bot.add_cog(RecommendationCog(bot))
+    logger = logging.getLogger("curatorchan")
+    logger.setLevel(logging.DEBUG)
+    logging.getLogger("discord.http").setLevel(logging.INFO)
+
+    handler = logging.handlers.RotatingFileHandler(
+        filename="discord.log",
+        encoding="utf-8",
+        maxBytes=32 * 1024 * 1024,  # 32 MiB
+        backupCount=5,  # Rotate through 5 files
+    )
+    dt_fmt = "%Y-%m-%d %H:%M:%S"
+    formatter = logging.Formatter(
+        "[{asctime}] [{levelname:<8}] {name}: {message}", dt_fmt, style="{"
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    await bot.add_cog(RecommendationCog(bot, logger))
