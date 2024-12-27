b3cbd6abda7de3cf3b4da3bfd376a7ad130b6fb4
Oliver Harcourt <96662442+oliverharcourt@users.noreply.github.com>
2024-12-27 01:25:50 +0100

    Add copyright and license notice

--- curatorchan/bot/recommend_cog.py
+++ curatorchan/bot/recommend_cog.py
@@ -1,3 +1,21 @@
+# Curator-chan
+# Copyright (C) 2024  Oliver Harcourt
+
+# This file is part of Curator-chan.
+
+# Curator-chan is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Curator-chan is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Curator-chan.  If not, see <https://www.gnu.org/licenses/>.
+
 import logging
 import time
 from typing import Literal, Optional
